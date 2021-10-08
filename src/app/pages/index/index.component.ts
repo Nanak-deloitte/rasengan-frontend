7b60081a1766162c632eea8ce1d7bd00ddbe32aa
Renu Joshi <rejoshi@deloitte.com>
2021-10-08 20:55:35 +0530

    merged dev (login component)

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import Chart from "chart.js";
 import noUiSlider from "nouislider";
 
 @Component({
@@ -20,6 +21,170 @@ export class IndexComponent implements OnInit, OnDestroy {
   ngOnInit() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.add("index-page");
+
+    
+    var canvas: any = document.getElementById("chartBig");
+    var ctx = canvas.getContext("2d");
+    var gradientFill = ctx.createLinearGradient(0, 350, 0, 50);
+    gradientFill.addColorStop(0, "rgba(228, 76, 196, 0.0)");
+    gradientFill.addColorStop(1, "rgba(228, 76, 196, 0.14)");
+    var chartBig = new Chart(ctx, {
+      type: "line",
+      responsive: true,
+      data: {
+        labels: [
+          "KUNA-1",
+          "KUNA-2",
+          "KUNA-3",
+          "KUNA-4",
+          "KUNA-5",
+          "KUNA-6",
+          "KUNA-7",
+          "KUNA-8",
+          "KUNA-9",
+          "KUNA-10"
+        ],
+        datasets: [
+          {
+            label: "Points",
+            fill: true,
+            backgroundColor: gradientFill,
+            borderColor: "#e44cc4",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#e44cc4",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#be55ed",
+            //pointHoverBorderColor:'rgba(35,46,55,1)',
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: [350, 1600, 2400, 2650, 2850, 3000, 3220, 3490, 4200, 4450],
+            tension: 0.4,
+          },
+          {
+            label: "Points",
+            fill: true,
+            backgroundColor: gradientFill,
+            borderColor: "#e44cc4",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#e44cc4",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#be55ed",
+            //pointHoverBorderColor:'rgba(35,46,55,1)',
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: [50, 1200, 2300, 2580, 2800, 3200 , 3400, 3850, 4100, 4500],
+            tension: 0.4,
+          },
+          {
+            label: "Points",
+            fill: true,
+            backgroundColor: gradientFill,
+            borderColor: "#e44cc4",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#e44cc4",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#be55ed",
+            //pointHoverBorderColor:'rgba(35,46,55,1)',
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: [200, 1500, 2100, 2600, 2850, 3050, 3500, 3750, 4050, 4350, 4650],
+            tension: 0.4,
+          },
+          {
+            label: "Points",
+            fill: true,
+            backgroundColor: gradientFill,
+            borderColor: "#e44cc4",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#e44cc4",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#be55ed",
+            //pointHoverBorderColor:'rgba(35,46,55,1)',
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: [100, 1600, 2200, 2550, 2950, 3280, 3450, 3790, 4300, 4570]
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: 'yellow',
+
+        tooltips: {
+          backgroundColor: "#fff",
+          titleFontColor: "#ccc",
+          bodyFontColor: "#666",
+          bodySpacing: 4,
+          xPadding: 12,
+          mode: "nearest",
+          intersect: 0,
+          position: "nearest"
+        },
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: "rgba(0,0,0,0.0)",
+                zeroLineColor: "transparent"
+              },
+              ticks: {
+                stepSize : 100,
+                display: false,
+                suggestedMin: 0,
+                suggestedMax: 350,
+                padding: 200,
+                fontColor: "#9a9a9a"
+              }
+            }
+          ],
+
+          xAxes: [
+            {
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: "rgba(0,0,0,0)",
+                zeroLineColor: "transparent"
+              },
+              ticks: {
+                padding: 20,
+                fontColor: "#9a9a9a"
+              }
+            }
+          ]
+        }
+      }
+    });
 
     var slider = document.getElementById("sliderRegular");
 
